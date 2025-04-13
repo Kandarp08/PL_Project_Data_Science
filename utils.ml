d1cba30c1cb1a97c0a9b29aebec5910a93c1983e
siddharthreddy <siddarth.maram@gmail.com>
2025-04-13 11:40:53 +0530

    Added utils.ml and utils.mli

---
+++ utils.ml
@@ -0,0 +1,10 @@
+let split_by_comma = String.split_on_char ','
+
+let strip str = 
+  let trimmed = String.trim str in
+  let l = String.length trimmed in
+  if l < 2 then trimmed
+  else 
+    if (String.starts_with ~prefix:"\"" trimmed && String.ends_with ~suffix:"\"" trimmed) || 
+      (String.starts_with ~prefix:"\'" trimmed && String.ends_with ~suffix:"\'" trimmed) then String.sub trimmed 1 (l-2)
+    else trimmed
