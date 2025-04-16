cb6b474a2ffe3c8c95a6a7ec42c90ff5b76b6f2b
Kandarp08 <Dave.Kandarp@iiitb.ac.in>
2025-04-16 11:58:58 +0530

    Deleted previous folder

--- kandarp/float_transformations.mli
+++
@@ -1,5 +0,0 @@
-(* Common transformations that can be applied on a column *)
-module type FLOAT_TRANSFORMATIONS = 
-sig
-    val normalize : (float Seq.t) -> (float Seq.t)
-end
--- kandarp/int_transformations.mli
+++
@@ -1,5 +0,0 @@
-(* Common transformations that can be applied on a column *)
-module type INT_TRANSFORMATIONS = 
-sig
-    val normalize : (int Seq.t) -> (float Seq.t)
-end
