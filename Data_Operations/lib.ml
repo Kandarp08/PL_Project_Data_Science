197a4dab378cff52bc2b0ebd8449295c07b54f1c
Kandarp08 <Dave.Kandarp@iiitb.ac.in>
2025-04-22 21:51:31 +0530

    Minor change in lib.ml

--- Data_Operations/lib.ml
+++ Data_Operations/lib.ml
@@ -17,34 +17,34 @@ end
 module Lib : LIB = 
 struct
 
+    let rec convert col_idx mapped_column original_column original_row = 
+                
+        match mapped_column () with 
+        | Seq.Nil -> Seq.Nil
+        | Seq.Cons(h', t') -> 
+            
+            match original_column () with
+            | Seq.Nil -> failwith "Unexpected error in Lib.map"
+            | Seq.Cons(h, t) ->
+
+                match original_row () with
+                | Seq.Nil -> failwith "Unexpected error in Lib.map"
+                | Seq.Cons(rowh, rowt) ->
+
+                    let old_row = ref (Array.of_list rowh) in
+                    !old_row.(col_idx) <- h';
+                    let new_row = (Array.to_list !old_row) in
+
+                    Seq.Cons(new_row, fun () -> convert col_idx t' t rowt)
+
     let map df f col_name = 
 
         let col_idx = Dataframe.get_column_index df col_name in
         let column = Dataframe.get_column df col_name in 
 
-        let mapped_column = Operations.map f column in 
-
-        let rec convert mapped_column original_column original_row = 
-            
-            match mapped_column () with 
-            | Seq.Nil -> Seq.Nil
-            | Seq.Cons(h', t') -> 
-                
-                match original_column () with
-                | Seq.Nil -> failwith "Unexpected error in Lib.map"
-                | Seq.Cons(h, t) ->
-
-                    match original_row () with
-                    | Seq.Nil -> failwith "Unexpected error in Lib.map"
-                    | Seq.Cons(rowh, rowt) ->
-
-                        let old_row = ref (Array.of_list rowh) in
-                        !old_row.(col_idx) <- h';
-                        let new_row = (Array.to_list !old_row) in
-
-                        Seq.Cons(new_row, fun () -> convert t' t rowt) in 
+        let mapped_column = Operations.map f column in
         
-        let new_df = { df with rows = fun () -> convert mapped_column column df.rows} in
+        let new_df = { df with rows = fun () -> convert col_idx mapped_column column df.rows} in
         
         new_df
 end
